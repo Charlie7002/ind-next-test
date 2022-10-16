cf4c5ede867d8a194c7e6f2fb9cfe9d1814c620c
chca <cachet.cha@gmail.com>
2022-10-16 18:27:02 +0200

    add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 const nextJest = require("next/jest");
 const createJestConfig = nextJest({
-	dir: "./",
+  dir: "./",
 });
 const customJestConfig = {
-	moduleDirectories: ["node_modules", "<rootDir>/"],
-	testEnvironment: "jest-environment-jsdom",
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
 module.exports = createJestConfig(customJestConfig);
